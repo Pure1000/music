c427cfc09f2253f7357d0d0fba558fe66654f4d4
Pure1000 <purebionest@gmail.com>
2025-04-12 17:28:07 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+input.onButtonPressed(Button.A, function () {
+    basic.pause(5000)
+})
 basic.showString("Music")
 basic.forever(function () {
     music.play(music.stringPlayable("C D E F G A B C5 ", 120), music.PlaybackMode.UntilDone)
